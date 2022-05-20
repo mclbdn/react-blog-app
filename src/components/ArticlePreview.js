10c0fac1fa2526e5fc5743d85b7ef5fad25e2eec
mclbdn <michalbednar93@gmail.com>
2022-05-20 18:03:52 -0500

    add article preview container

--- src/components/ArticlePreview.js
+++ src/components/ArticlePreview.js
@@ -8,15 +8,17 @@ const ArticlePreview = () => {
         <img src={img}></img>
       </div>
       <div className={styles.right_side_preview}>
-        <h2>Why Do Cats Have Whiskers?</h2>
-        <p>Elisabeth Strain | 22. 5. 2022</p>
-        <p>
+        <h4>Why Do Cats Have Whiskers?</h4>
+        <p className={styles.name_and_date}>Elisabeth Strain | 22. 5. 2022</p>
+        <p className={styles.perex}>
           A cat's whiskers — or vibrissae — are a well-honed sensory tool that helps a cat see in the dark and steer clear of hungry predators.
           Whiskers are highly sensitive tactile hairs that grow in patterns on a cat's muzzle, above its eyes and elsewhere on its body, like the
           ears, jaw and forelegs
         </p>
-        <a href="">Read whole article</a>
-        <p>0 comments</p>
+        <div className={styles.link_and_comments}>
+          <a href="">Read whole article</a>
+          <p>0 comments</p>
+        </div>
       </div>
     </div>
   );
